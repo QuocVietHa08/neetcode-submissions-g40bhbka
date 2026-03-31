5447c4042b07be8032de782b03d4599086b5746d
Edward Ha <56016006+QuocVietHa08@users.noreply.github.com>
2026-03-31 11:25:49 +0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        // this is two pointer problem
+        // the one the left and the one right
+        // l < r and nums[l] < nums[r] => sum = nums[r] = nums[l]
+    
+        let profit = 0;
+        let l = 0, r = prices.length -1;
+
+        while (l < r) {
+            let value = prices[r] - prices[l];
+            console.log(l, r, value, prices[r], prices[l])
+            if (prices[l] > prices[r]) l++
+            else r--;
+            profit = Math.max(profit, value)
+        }
+
+        return profit;
+    }
+}
