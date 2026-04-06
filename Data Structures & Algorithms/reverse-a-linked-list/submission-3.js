124d22d9bda72be0f1de33dfc09c2a1c8c90bb9a
Edward Ha <56016006+QuocVietHa08@users.noreply.github.com>
2026-04-06 23:12:54 +0700

    Add: reverse-a-linked-list - submission-3

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-3.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head) {
+        // need a function to reverser the string from top to bottom 
+        let result = null
+        let temp = head
+        while (temp) {
+            let next = temp.next
+            temp.next = result
+            result = temp
+            temp = next
+        }
+
+        return result
+    }
+}
