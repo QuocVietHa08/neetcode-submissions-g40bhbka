9a1cef7c71ecc868162eea0edb8a253eb3b956ce
Edward Ha <56016006+QuocVietHa08@users.noreply.github.com>
2026-04-06 09:31:00 +0700

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        // two pointer, loop thought each item using for and have an item as j
+        // loop through each combinatin and return the maxium value
+        // the maxium value = w * h
+        // w = r-l
+        // h = min(heights[j], heights[i])
+
+        let result = 0;
+        let l = 0, r = heights.length -1;
+        while (l < r) {
+            let value = (r-l) * Math.min(heights[l], heights[r])
+            result = Math.max(value, result)
+            if (heights[l] < heights[r]) l++
+            else r--
+        }
+
+        return result
+    }
+}
