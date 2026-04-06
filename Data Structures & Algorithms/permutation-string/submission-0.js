775cc5a01cd48a4ee2bc84a0713699bc4ce45a5a
Edward Ha <56016006+QuocVietHa08@users.noreply.github.com>
2026-04-06 09:44:12 +0700

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        // substring cab is a premutation of abc and it present in lecabee
+        // find all the premutation of a word and use string contain
+        // sort s1 and sort s2 then check the substring
+        let sort1 = s1.split('').sort().join('')
+        for (let i = 0; i < s2.length; i++) {
+            let j = i + s1.length;
+            let subString = s2.slice(i, j)
+             console.log('hello', subString, i, j)
+            subString.split('').sort().join('')
+            console.log('hello', subString)
+            if (subString == sort1) return true
+        }
+
+        return false
+    }
+}
