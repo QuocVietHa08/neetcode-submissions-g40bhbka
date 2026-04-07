d2d5bcfe6f2e7a19a863249efda11d0bf12c820f
Edward Ha <56016006+QuocVietHa08@users.noreply.github.com>
2026-04-07 10:56:46 +0700

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        // burce force
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] === target) return [i, j]
+            }
+        }
+
+        return []
+    }
+}
