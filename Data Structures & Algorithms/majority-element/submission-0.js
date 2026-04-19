b0c7f05f103a1ceaeac7716ca4ee926856c7302d
Edward Ha <56016006+QuocVietHa08@users.noreply.github.com>
2026-04-19 21:00:59 +0700

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let map = {}
+        for (let i = 0; i < nums.length;i++) {
+            map[nums[i]] = (map[nums[i]] || 0) +1
+            console.log(map)
+            if (map[nums[i]] > (nums.length/2)) return nums[i]
+        }
+        return null
+    }
+}
