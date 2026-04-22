0ea68e1954534049e9052cc590e0f52e54f8c7e8
Edward Ha <56016006+QuocVietHa08@users.noreply.github.com>
2026-04-23 00:35:55 +0700

    Add: find-anagram-mappings - submission-3

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    anagramMappings(nums1, nums2) {
+        let map = {}
+        for (let i = 0; i < nums2.length;i++) {
+            map[nums2[i]] = i
+        }
+
+        let result = []
+        for (let i = 0; i < nums1[i]; i++) {
+            let a = map[nums1[i]];
+            result.push(a)
+        }
+
+        return result
+    }
+}
