d4e030fd1cea15156820b095c8a25586a7d8cb4c
Edward Ha <56016006+QuocVietHa08@users.noreply.github.com>
2026-06-01 08:43:21 +0700

    Add: arranging-coins - submission-2

---
+++ Data Structures & Algorithms/arranging-coins/submission-2.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    arrangeCoins(n) {
+        // we have n coins and we have to build a stair case.
+        // first row will be: 1:
+        // second row will be: 2
+        // third row will be: 3
+        // four row will be: 4
+        // fifth row will be: 5
+        // .... -> return the complete row base on the n coint
+        // n + 0 / 2 = x then calculate with x is how many coin that need to handle 
+        // example we have 4 rows will need 
+        return Math.floor(Math.sqrt(2 * n + 0.25) - 0.5)
+        let l = 0;
+        let r = n;
+        let ans = 0
+        while (l <= r) {
+            let mid = Math.floor((l + r)/2);
+            let totalNeed = (mid * (mid+1))/2
+            if (totalNeed <= n) {
+                ans = mid;
+                l = mid +1;
+            } else {
+                r = mid -1
+            }
+        }
+
+        return ans
+
+
+    }
+}
