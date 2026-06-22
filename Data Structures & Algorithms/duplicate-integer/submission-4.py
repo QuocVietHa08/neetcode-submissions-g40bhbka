df85f3269c7809c56b153574c9af4c92e2d9c639
Edward Ha <56016006+QuocVietHa08@users.noreply.github.com>
2026-06-22 08:18:50 +0700

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # in js we add to a set and compare it length
+        # now in normal way with python how do we handle that 
+        # we take each item and compare it to the res of the array 
+        # if we exist then we will return the arryay using 2 loop
+        for i in range(len(nums)):
+            current_element = nums[i]
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+        
